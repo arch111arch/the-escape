6b1baee15e0791c1d693c7628c57b188a2b3d264
Gabriel Pettersson <gabriel.pettersson77@gmail.com>
2023-07-08 08:58:31 +0000

    Create base star message. Add inventory, logo and import termcolor and time.

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+from termcolor import colored, cprint
+import time
+
+inventory_dict = {}
+delay = 1
+logo = (colored('THE ESCAPE', 'yellow', attrs=['bold']))
+user_command = ''
+
+
+
+
+
+print('Welcome to the text-adventure ' + logo)
+print('')
+
+user_name = input('What is your name, adventurer? > ')
+print('') 
+print('Good to meet you ' + (colored(user_name, 'green')) 
++ ', now let us begin.\n')
+
+
+
+
